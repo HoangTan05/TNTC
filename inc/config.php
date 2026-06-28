4e37cd8b92cba6ef32b4cd0a13ea326a9372c0b8
HoangTan05 <nguyentannt362@gmail.com>
2026-06-28 17:27:19 +0700

    Fix

--- inc/config.php
+++ inc/config.php
@@ -1,5 +1,4 @@
 <?php
-// 1. Tự động kiểm tra: Nếu có biến môi trường của Render/Railway thì dùng, không thì dùng local
 if (getenv('DB_HOST')) {
     $db_host = getenv('DB_HOST');
     $db_user = getenv('DB_USER');
@@ -13,25 +12,23 @@ if (getenv('DB_HOST')) {
     $db_name = $_ENV['DB_NAME'];
     $db_port = $_ENV['DB_PORT'];
 } else {
-    // Cấu hình cứng trực tiếp thông số Railway của bạn để ÉP hệ thống kết nối thẳng
-    $db_host = 'viaduct.proxy.rlwy.net';
+    // THÔNG SỐ PUBLIC (Dùng cho Render và Navicat kết nối từ ngoài vào)
+    $db_host = 'viaduct.proxy.rlwy.net'; // Dùng viaduct thay vì reseau
     $db_user = 'root';
     $db_pass = 'LsGwcFSRVjZiWZebxyAZuKvWeITYQLzE'; 
     $db_name = 'railway';
-    $db_port = 21925; // Số port public trên hình của bạn
+    $db_port = 14125; // Cổng công khai chuẩn từ ảnh đầu tiên của bạn
 }
 
-// 2. Bật báo lỗi chuẩn cho MySQLi
+// Bật báo lỗi chuẩn cho MySQLi
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 try {
-    // 3. Kết nối Database
     $conn = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
     $conn->set_charset("utf8mb4");
 } catch (mysqli_sql_exception $e) {
     die("Lỗi kết nối Database rồi: " . $e->getMessage());
 }
 
-// 4. Biến base_url tự động định tuyến đường dẫn
 $base_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'];
 ?>
